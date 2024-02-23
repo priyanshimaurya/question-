1eab203a697a9e6dc0559d5878b5ce7d34ce36db
jyoti maurya <jyotiangel1993@gmail.com>
2024-02-23 11:34:58 +0530

    call by value added

---
+++ call_by_val.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void swap(int a,int b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    // we can not change value in main 
+    cout<<"inside the function :-"<<a<<" "<<b<<endl;
+}
+int main(){
+    int x=10,y=20;
+    cout<<"before swaping value is :- "<<x<<" "<<y<<endl;
+    swap(x,y);
+    cout<<"After swaping value is :- "<<x<<" "<<y <<endl;
+    return 0;
+}
